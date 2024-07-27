548c2dadf7ef45ac3530d440dd947ab190e6799b
yasirIrfan <yasirirfan@gmai.com>
2024-07-27 19:08:51 +0500

    Added new endpoint to get string.

--- ThirdTryOfASPNETCOREWEBAPI/Controllers/StudentAPIController.cs
+++ ThirdTryOfASPNETCOREWEBAPI/Controllers/StudentAPIController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Text;
 using ThirdTryOfASPNETCOREWEBAPI.Models;
 
 namespace ThirdTryOfASPNETCOREWEBAPI.Controllers
@@ -104,12 +105,6 @@ namespace ThirdTryOfASPNETCOREWEBAPI.Controllers
             return Ok(student);
         }
 
-
-
-
-
-
-
         [HttpDelete]
         [Route("DeleteStudentById/{id}")]
         public async Task<ActionResult<Student>> DeleteStudent(int id)
@@ -124,103 +119,17 @@ namespace ThirdTryOfASPNETCOREWEBAPI.Controllers
             return Ok();
 
         }
+
+
+        [HttpGet]
+        [Route("GetStringTest")]
+        public string GetStringTest()
+        {
+            return string.Empty;
+        }
+
+
+
     }
 }
-
-                        ////  Changing the EndPoints names Here 
-
-//using Microsoft.AspNetCore.Mvc;
-//using Microsoft.EntityFrameworkCore;
-//using ThirdTryOfASPNETCOREWEBAPI.Models;
-//using System.Collections.Generic;
-//using System.Threading.Tasks;
-
-//namespace ThirdTryOfASPNETCOREWEBAPI.Controllers
-//{
-//    [Route("api/[controller]")]
-//    [ApiController]
-//    public class StudentAPIController : ControllerBase
-//    {
-//        private readonly MydbContext context;
-
-//        public StudentAPIController(MydbContext context)
-//        {
-//            this.context = context;
-//        }
-
-//        /// <summary>
-//        /// Retrieves all students.
-//        /// </summary>
-//        [HttpGet]
-//        [Route("GetAllStudents")]
-//        public async Task<ActionResult<List<Student>>> GetAllStudents()
-//        {
-//            var data = await context.Students.ToListAsync();
-//            return Ok(data);
-//        }
-
-//        /// <summary>
-//        /// Retrieves a student by ID.
-//        /// </summary>
-//        [HttpGet("{id}")]
-//        [Route("GetStudentById/{id}")]
-//        public async Task<ActionResult<Student>> GetStudentById(int id)
-//        {
-//            var student = await context.Students.FindAsync(id);
-//            if (student == null)
-//            {
-//                return NotFound();
-//            }
-//            return Ok(student);
-//        }
-
-//        /// <summary>
-//        /// Inserts a new student.
-//        /// </summary>
-//        [HttpPost]
-//        [Route("InsertStudent")]
-//        public async Task<ActionResult<Student>> InsertStudent(Student std)
-//        {
-//            await context.Students.AddAsync(std);
-//            await context.SaveChangesAsync();
-//            return Ok(std);
-//        }
-
-//        /// <summary>
-//        /// Updates a student by ID.
-//        /// </summary>
-//        [HttpPut("{id}")]
-//        [Route("UpdateStudentById/{id}")]
-//        public async Task<ActionResult<Student>> UpdateStudentById(int id, Student std)
-//        {
-//            context.Entry(std).State = EntityState.Modified;
-//            await context.SaveChangesAsync();
-//            return Ok(std);
-//        }
-
-//        /// <summary>
-//        /// Deletes a student by ID.
-//        /// </summary>
-//        [HttpDelete("{id}")]
-//        [Route("DeleteStudentById/{id}")]
-//        public async Task<ActionResult<Student>> DeleteStudentById(int id)
-//        {
-//            var std = await context.Students.FindAsync(id);
-//            if (std == null)
-//            {
-//                return NotFound();
-//            }
-//            context.Students.Remove(std);
-//            await context.SaveChangesAsync();
-//            return Ok();
-//        }
-//    }
-
-
-
-
-
-
-
-
 
